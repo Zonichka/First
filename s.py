1916a627314c9babb56260fe4107a1076299712f
Sofia Golovina <65851895+Zonichka@users.noreply.github.com>
2022-02-10 11:33:19 +0300

    Update s.py

--- s.py
+++ s.py
@@ -6,14 +6,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
-  #conn, addr = s.accept()
-  child = os.fork()
-  if child == 0:
-    data = conn.recv(1024)
-	  conn.send(data.upper())
-	  conn.close()
-	  sys.exit()
-  else:
-	  conn.close()
+	#conn, addr = s.accept()
+	child = os.fork()
+	if child == 0:
+		data = conn.recv(1024)
+		conn.send(data.upper())
+		conn.close()
+		sys.exit()
+  	else:
+		conn.close()
 
 server_socket.close()
