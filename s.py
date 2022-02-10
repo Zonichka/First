e7e37790eb800cd07028892ac6d3031c5025358f
Sofia Golovina <65851895+Zonichka@users.noreply.github.com>
2022-02-10 11:21:48 +0300

    Create s.py

---
+++ s.py
@@ -0,0 +1,19 @@
+import socket
+import os
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+  #conn, addr = s.accept()
+  child = os.fork()
+  if child == 0:
+    data = conn.recv(1024)
+	  conn.send(data.upper())
+	  conn.close()
+	  sys.exit()
+  else:
+	  conn.close()
+
+server_socket.close()
