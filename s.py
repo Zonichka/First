d7b504bc5136dd95037eb3a737c1279aacf46f49
Sofia Golovina <65851895+Zonichka@users.noreply.github.com>
2022-02-10 12:48:43 +0300

    Update s.py

--- s.py
+++ s.py
@@ -2,18 +2,23 @@ import socket
 import os
 import sys
 
+
+def server(conn):
+    while True:
+        data = conn.recv(1024)
+        if (data == b'close') or (not data):
+            break
+        conn.send(data)
+    conn.close()
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
-	conn, addr = s.accept()
-	child = os.fork()
-	if child == 0:
-		data = conn.recv(1024)
-		conn.send(data.upper())
-		conn.close()
-		sys.exit()
-  	else:
-		conn.close()
+    conn, addr = s.accept()
+    print("Connection", addr)
+    t = threading.Thread(target=server(conn), args=(conn, addr))
+    t.start()
 
 s.close()
